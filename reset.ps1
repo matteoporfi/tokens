c46d1016661db83f4a43a1f659dd3d914d501d82
matteoporfi <matteopofirio84@gmail.com>
2025-03-28 09:38:21 +0100

    Create reset.ps1

---
+++ reset.ps1
@@ -0,0 +1,32 @@
+#Hides Desktop Icons
+$Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+Set-ItemProperty -Path $Path -Name "HideIcons" -Value 0
+
+#Changes Background  
+#URL For the Image of your choice (Wanna Cry Ransomware Background)
+$url = "https://wallpapercave.com/wp/wp5493583.jpg"
+
+
+Invoke-WebRequest $url -OutFile C:\temp\test.jpg
+
+
+$setwallpapersrc = @"
+using System.Runtime.InteropServices;
+
+public class Wallpaper
+{
+  public const int SetDesktopWallpaper = 20;
+  public const int UpdateIniFile = 0x01;
+  public const int SendWinIniChange = 0x02;
+  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+  private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+  public static void SetWallpaper(string path)
+  {
+    SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
+  }
+}
+"@
+Add-Type -TypeDefinition $setwallpapersrc
+
+[Wallpaper]::SetWallpaper("C:\temp\test.jpg")
+
