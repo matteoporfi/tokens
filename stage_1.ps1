cfe6602543a94b7ea14c9f22888b201ab0c95885
matteoporfi <matteopofirio84@gmail.com>
2025-04-03 14:51:34 +0200

    Update stage_1.ps1

--- stage_1.ps1
+++ stage_1.ps1
@@ -1,6 +1,6 @@
 # Define the URL and output file path
-$PdfUrl = "https://raw.githubusercontent.com/matteoporfi/tokens/refs/heads/main/sample-1.pdf" 
-$OutputPath = "$env:TEMP\Liste des particpants.pdf"
+$PdfUrl = "https://raw.githubusercontent.com/matteoporfi/tokens/refs/heads/main/sample-2.pdf" 
+$OutputPath = "$env:TEMP\Flyer Cybersécurité VF1.pdf"
 
 # Download the PDF
 Invoke-WebRequest -Uri $PdfUrl -OutFile $OutputPath
